dd565c88220f77e91172c7e1d6f9d5d9b3b2d5e1
MARTIN GENET <martingenet@MHG.local>
2018-03-13 15:28:28 -0400

    add firebase api keys, material design

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCOoK0SfIi1I93NXLu3cxZ9koFWyilgXTM",
+    authDomain: "ng-final-project-mhg.firebaseapp.com",
+    databaseURL: "https://ng-final-project-mhg.firebaseio.com",
+    projectId: "ng-final-project-mhg",
+    storageBucket: "ng-final-project-mhg.appspot.com",
+    messagingSenderId: "76175965005"
+  }
 };
