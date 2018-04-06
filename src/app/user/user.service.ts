d6b52e0a79ecafb2be98e8f7335e9e9abb62c274
MARTIN GENET <martingenet@MHG.local>
2018-04-06 18:50:34 -0400

    fixing issues w single user details

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,21 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   AngularFirestore,
-  AngularFireStoreCollection,
-  AngularFireStoreDocument
-} from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from "angularfire2/firestore";
+import { Observable } from "rxjs/Observable";
 
-import { User } from './user.model';
+import { User } from "./user.model";
 
 @Injectable()
 export class UserService {
   userCollection: AngularFirestoreCollection<User>;
+  userDoc: AngularFirestoreDocument<User>
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore) {}
+
 
     getUsers() {
       this.userCollection = this.afs.collection('users');
       return this.userCollection.valueChanges();
+    }
+
+    getUser(id: string) {
+      this.userDoc = this.afs.doc<User>(`users/${id}`)
+      return this.userDoc.valueChanges();
     }
 }
