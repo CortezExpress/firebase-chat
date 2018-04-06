4ba015148f04d19490ea7cf769c30353b153f766
MARTIN GENET <martingenet@MHG.local>
2018-04-06 18:14:55 -0400

    added some user profiles

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
-
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatInputModule],
-  exports: [MatButtonModule, MatIconModule, MatInputModule]
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule
+  ]
 })
 export class MaterialModule { }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -11,13 +11,15 @@ import { UserListItemComponent } from './user-list-item/user-list-item.component
 const routes: Routes = [
   { path: 'me', component: UserDashboardComponent, data: { title: "Dashboard" }},
   { path: 'users', component: UserListComponent, data: { title: "Users" }},
-  { path: 'profile', component: UserDetailComponent, data: { title: "Profile" }},
+  { path: 'users/:id', component: UserDetailComponent, data: { title: "Profile" }},
 ]
+
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild(routes)
   ],
+  exports: [UserListItemComponent],
   declarations: [UserDashboardComponent, UserDetailComponent, UserListComponent, UserListItemComponent],
   providers: [UserService]
 })
