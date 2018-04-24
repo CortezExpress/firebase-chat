65ef79df00f6da3bd17ad9b2b176bdf7178b820d
MARTIN GENET <martingenet@MHG.local>
2018-04-24 10:37:36 -0400

    added update email

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,19 +3,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
--- src/app/user/user-dashboard/user-dashboard.component.ts
+++ src/app/user/user-dashboard/user-dashboard.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../core/auth.service';
+import { User } from '../user.model';
+import { UserService } from "../user.service";
+
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
   styleUrls: ['./user-dashboard.component.css']
 })
 export class UserDashboardComponent implements OnInit {
+  editing = false;
 
-  constructor() { }
+  user: User;
+  constructor(private auth: AuthService, private userService: UserService) { }
 
   ngOnInit() {
+    this.getUser()
   }
 
+  getUser() {
+    return this.auth.user.subscribe(user => (this.user = user))
+  }
+
+  updateProfile() {
+    return this.userService.updateProfileData(
+      this.user.displayName,
+      this.user.photoURL
+  )
+  }
+
+  updateEmail() {
+    return this.userService.updateEmailData(this.user.email)
+  }
 }
